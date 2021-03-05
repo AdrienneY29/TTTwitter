bb6d4894d4fd07346667e3c981627fcf7e8d6350
AdrienneY29 <55600245+AdrienneY29@users.noreply.github.com>
2021-03-04 20:34:57 -0600

    3rd commit: followers/following/hash tags in this commit

--- src/main/java/com/tts/techtalenttwitter/controller/TweetController.java
+++ src/main/java/com/tts/techtalenttwitter/controller/TweetController.java
@@ -1,5 +1,6 @@
 package com.tts.TechTalentTwitter.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.validation.Valid;
@@ -11,23 +12,46 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.tts.TechTalentTwitter.model.Tag;
 import com.tts.TechTalentTwitter.model.Tweet;
+import com.tts.TechTalentTwitter.model.TweetDisplay;
 import com.tts.TechTalentTwitter.model.User;
+import com.tts.TechTalentTwitter.repository.TagRepository;
 import com.tts.TechTalentTwitter.service.TweetService;
 import com.tts.TechTalentTwitter.service.UserService;
 
 @Controller
 public class TweetController {
+
     @Autowired
     private UserService userService;
 
     @Autowired
     private TweetService tweetService;
 
+    @Autowired
+    private TagRepository tagRepository;
+
     @GetMapping(value= {"/tweets", "/"})
-    public String getFeed(Model model){
-        List<Tweet> tweets = tweetService.findAll();
+    public String getFeed(@RequestParam(value="filter", required=false) String filter, Model model){
+        User loggedInUser = userService.getLoggedInUser();
+        List<TweetDisplay> tweets = new ArrayList<>();
+
+        if (filter == null) {
+            filter = "all";
+        }
+        if (filter.equalsIgnoreCase("following")) {
+            List<User> following = loggedInUser.getFollowing();
+            tweets = tweetService.findAllByUsers(following);
+            model.addAttribute("filter", "following");
+        }
+        else {
+            tweets = tweetService.findAll();
+            model.addAttribute("filter", "all");
+        }
+
         model.addAttribute("tweetList", tweets);
         return "feed";
     }
@@ -49,13 +73,19 @@ public class TweetController {
         }
         return "newTweet";
     }
+
     @GetMapping(value = "/tweets/{tag}")
     public String getTweetsByTag(@PathVariable(value="tag") String tag, Model model) {
-        List<Tweet> tweets = tweetService.findAllWithTag(tag);
+        List<TweetDisplay> tweets = tweetService.findAllWithTag(tag);
         model.addAttribute("tweetList", tweets);
         model.addAttribute("tag", tag);
         return "taggedTweets";
     }
 
-
+    @GetMapping(value = "/tags")
+    public String getTags(Model model) {
+        List<Tag> tag = (List<Tag>)tagRepository.findAll();
+        model.addAttribute("tagList", tag);
+        return "tags";
+    }
 }
--- src/main/java/com/tts/techtalenttwitter/controller/UserController.java
+++ src/main/java/com/tts/techtalenttwitter/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.tts.TechTalentTwitter.controller;
 
 import com.tts.TechTalentTwitter.model.Tweet;
+import com.tts.TechTalentTwitter.model.TweetDisplay;
 import com.tts.TechTalentTwitter.model.User;
 import com.tts.TechTalentTwitter.service.TweetService;
 import com.tts.TechTalentTwitter.service.UserService;
@@ -10,7 +11,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -25,9 +28,10 @@ public class UserController {
 
     @GetMapping(value = "/users/{username}")
     public String getUser(@PathVariable(value = "username") String username, Model model) {
-        User loggedInUser = userService.getLoggedInUser();
         User user = userService.findByUsername(username);
-        List<Tweet> tweets = tweetService.findAllByUser(user);
+        List<TweetDisplay> tweets = tweetService.findAllByUser(user);
+
+        User loggedInUser = userService.getLoggedInUser();
         List<User> following = loggedInUser.getFollowing();
         boolean isFollowing = false;
         for (User followedUser : following) {
@@ -47,21 +51,39 @@ public class UserController {
 
     //add more  code here
     @GetMapping(value = "/users")
-    public String getUsers(Model model) {
-        List<User> users = userService.findAll();
+    public String getUsers(@RequestParam(value = "filter", required = false) String filter, Model model) {
+        List<User> users = new ArrayList<User>();
+
+        //this gets a list of user's followers
         User loggedInUser = userService.getLoggedInUser();
+
         List<User> usersFollowing = loggedInUser.getFollowing();
+        List<User> usersFollowers = loggedInUser.getFollowers();
+        if (filter == null) {
+            filter = "all";
+        }
+        //this is case for getting followers then getting users that user is following
+        if (filter.equalsIgnoreCase("followers")) {
+            users = usersFollowers;
+            model.addAttribute("filter", "followers");
+        } else if (filter.equalsIgnoreCase("following")) {
+            users = usersFollowing;
+            model.addAttribute("filter", "following");
+        } else {
+            users = userService.findAll();
+            model.addAttribute("filter", "all");
+        }
+        model.addAttribute("users", users);
+
+        SetTweetCounts(users, model);
         SetFollowingStatus(users, usersFollowing, model);
 
-
-        model.addAttribute("users", users);
-        SetTweetCounts(users, model);
         return "users";
     }
     private void SetTweetCounts(List<User> users, Model model) {
         HashMap<String,Integer> tweetCounts = new HashMap<>();
         for (User user : users) {
-            List<Tweet> tweets = tweetService.findAllByUser(user);
+            List<TweetDisplay> tweets = tweetService.findAllByUser(user);
             tweetCounts.put(user.getUsername(), tweets.size());
         }
         model.addAttribute("tweetCounts", tweetCounts);
